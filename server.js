dbca78fe0f2fd927bbae2a0cc75e1bbd192e5ef2
rcoo4 <129907520+rcoo4@users.noreply.github.com>
2026-06-01 11:02:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -60,10 +60,7 @@ async function processImage(imageBuffer, options){
     let processedImage = imageBuffer;
 
     if (options.width || options.height){
-        processedImage = await sharp(imageBuffer)
-    .resize(options.width, options.height, { fit: 'cover' })
-    .jpeg({ quality: options.quality })
-    .toBuffer();
+        processedImage = await sharp(imageBuffer).resize(options.width, options.height, { fit: 'cover' }).jpeg({ quality: options.quality }).toBuffer();
     }
     return processedImage;
 }
